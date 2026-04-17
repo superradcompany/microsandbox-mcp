4f8a92749302f52911034e7c1c21fe4abc124525
Stephen Akinyemi <appcypher@outlook.com>
2026-04-18 00:34:45 +0100

    chore: bump version to 0.3.14 and update microsandbox dependency

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { registerMetricsTools } from "./tools/metrics.js";
 
 const server = new McpServer({
   name: "microsandbox",
-  version: "0.3.13",
+  version: "0.3.14",
 });
 
 registerSandboxTools(server);
