bb94be5372bf45427cde3641f1080008618b4c1d
Stephen Akinyemi <appcypher@outlook.com>
2026-04-29 22:32:46 +0100

    chore: bump version to 0.4.1

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { registerMetricsTools } from "./tools/metrics.js";
 
 const server = new McpServer({
   name: "microsandbox",
-  version: "0.4.0",
+  version: "0.4.1",
 });
 
 registerSandboxTools(server);
