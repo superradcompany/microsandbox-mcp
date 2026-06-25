157c1627bd84f118be506bc7d90f2c8404b4e4a3
Stephen Akinyemi <appcypher@outlook.com>
2026-06-25 15:57:04 +0100

    chore(microsandbox): bump release refs to 0.5.11

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import { registerSshTools } from "./tools/ssh.js";
 
 const server = new McpServer({
   name: "microsandbox",
-  version: "0.5.10",
+  version: "0.5.11",
 });
 
 registerResources(server);
