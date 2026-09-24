793bb19568e7ecf4ea36bb542077b70df7ea177a
Tochukwu Nkemdilim <11903253+toksdotdev@users.noreply.github.com>
2026-09-24 07:33:36 -0400

    chore(release): prepare MCP v0.7.3

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import { registerSshTools } from "./tools/ssh.js";
 
 const server = new McpServer({
   name: "microsandbox",
-  version: "0.7.2",
+  version: "0.7.3",
 });
 
 registerResources(server);
